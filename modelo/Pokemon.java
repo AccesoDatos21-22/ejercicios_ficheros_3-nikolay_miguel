df42ecf1965e8771075c41e42ec86dfd7ee62968
migerusama <migueldepablos99@gmail.com>
2021-10-11 19:07:58 +0200

    ejercicio12

--- modelo/Pokemon.java
+++ modelo/Pokemon.java
@@ -1,5 +1,100 @@
 package modelo;
 
-public class Pokemon {
+import java.io.Serializable;
 
+public class Pokemon implements Serializable {
+
+    protected String name;
+    protected int level, life, attack, defense, specialAttack, specialDefense, speed;
+
+
+    public Pokemon() {
+        super();
+    }
+
+    public Pokemon(String name, int level, int life, int attack, int defense, int specialAttack, int specialDefense, int speed) {
+        this.name = name;
+        this.level = level;
+        this.life = life;
+        this.attack = attack;
+        this.defense = defense;
+        this.specialAttack = specialAttack;
+        this.specialDefense = specialDefense;
+        this.speed = speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getSpecialAttack() {
+        return specialAttack;
+    }
+
+    public void setSpecialAttack(int specialAttack) {
+        this.specialAttack = specialAttack;
+    }
+
+    public int getSpecialDefense() {
+        return specialDefense;
+    }
+
+    public void setSpecialDefense(int specialDefense) {
+        this.specialDefense = specialDefense;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    @Override
+    public String toString() {
+        return "Pokemon{" +
+                "name='" + name + '\'' +
+                ", level=" + level +
+                ", life=" + life +
+                ", attack=" + attack +
+                ", defense=" + defense +
+                '}';
+    }
 }
