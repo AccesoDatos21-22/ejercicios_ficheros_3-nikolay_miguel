b74fbbce64adfb90e2a074981c8ed822d94a8956
migerusama <migueldepablos99@gmail.com>
2021-10-11 18:52:39 +0200

    ejercicio10

--- ficheros/Ejercicios1_3.java
+++ ficheros/Ejercicios1_3.java
@@ -1,5 +1,6 @@
 package ficheros;
 
+import java.io.*;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Scanner;
@@ -7,59 +8,64 @@ import java.util.Scanner;
 import interfaces.InterfazEjercicios1_3;
 
 /**
- * 
  * @author Escribe_aqui_tu_nombre
- * @date 
- * @version 
+ * @date
  * @license GPLv3
- *
  */
 public class Ejercicios1_3 implements InterfazEjercicios1_3 {
 
-	
-	
-	@Override
-	public List<String> getFrases(Scanner escaner) {
-		
-		return null;
-	}
 
-	@Override
-	public Path getNombre(Scanner escaner) {
-		
-		return null;
-	}
-	
-	@Override
-	public void escribefrases(List<String> cadenas, Path ruta) {
-		
-		
-	}
+    @Override
+    public List<String> getFrases(Scanner escaner) {
 
-	@Override
-	public void leerFrases(Path ruta) {
-		// TODO Auto-generated method stub
-		
-	}
+        return null;
+    }
+
+    @Override
+    public Path getNombre(Scanner escaner) {
+
+        return null;
+    }
+
+    @Override
+    public void escribefrases(List<String> cadenas, Path ruta) {
 
 
+    }
 
-	@Override
-	public void escribirFlotante(float numeroDecimal, String ruta) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void leerFrases(Path ruta) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public List<Float> leerFlotante(String ruta) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    }
 
-	
-	
-		
-		
+
+    @Override
+    public void escribirFlotante(float numeroDecimal, String ruta) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void imprimirFlotante(String ruta) {
+        try (DataInputStream dis = new DataInputStream(new FileInputStream(ruta))){
+            while (true){
+                System.out.println(dis.readFloat());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }catch (EOFException e) {
+            System.out.println("Fin de fichero");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Float> leerFlotante(String ruta) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 
 }
